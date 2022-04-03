24e9c963c66db13dedb3b740dd1ce32b00392f1e
Ashutosh rawat <87859323+ashu540@users.noreply.github.com>
2022-04-03 11:40:14 +0530

    Create lec 1.py

---
+++ lec 1.py
@@ -0,0 +1,67 @@
+print("hello world")
+
+"""
+output-->
+hello world
+
+"""
+
+
+______________________________________________
+
+print("my name is")
+print("ashutosh rawat")
+
+
+output-->
+my name is
+ashutosh rawat
+
+
+______________________________________________
+
+print("my name is",end=" ")
+print("ashutosh rawat")
+
+
+output-->
+my name is ashutosh rawat
+
+______________________________________________
+
+print("my name is","ashutosh rawat")
+#automaticaly space will be add in this case
+
+output-->
+my name is ashutosh rawat
+
+
+______________________________________________
+
+print("my name is","\n","ashutosh rawat")
+#automaticaly space will be add in this case
+
+output-->
+my name is 
+ ashutosh rawat
+  
+______________________________________________
+
+
+print("my name is \\n ashutosh rawat")
+print("my name is \\t ashutosh rawat")
+
+
+output-->
+my name is \n ashutosh rawat
+my name is \t ashutosh rawat
+  
+
+_____________________________________________
+
+for single line comment-----> #
+for multiline   comment-----> """ """
+
+
+
+
