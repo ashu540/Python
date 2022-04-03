7fe2022eb8fa5d979d5034f3e1331b8590c6c18e
Ashutosh rawat <87859323+ashu540@users.noreply.github.com>
2022-04-03 12:31:40 +0530

    Create projec1.py

---
+++ projec1.py
@@ -0,0 +1,22 @@
+# calculator for add two numbers
+print("Enter the number1")
+
+num1 = input()
+
+print("Enter the number2")
+
+num2 = input()
+
+print("result after add the numbers", int(num1)+int(num2))
+
+
+"""
+output-->
+
+Enter the number1
+56
+Enter the number2
+4
+result after add the numbers 60
+
+"""
