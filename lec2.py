dd0aa9b77c1f99bfa3a3a2ca034ecc1aff55c836
Ashutosh rawat <87859323+ashu540@users.noreply.github.com>
2022-04-03 12:28:43 +0530

    Create lec2.py

---
+++ lec2.py
@@ -0,0 +1,63 @@
+var1 ="hello world"
+var2=7
+var3=77.9
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+
+"""
+output-->
+
+<class 'str'>
+<class 'int'>
+<class 'float'>
+
+"""
+#_________________________________________________________________________________________________
+
+var1 ="54"
+var2=7
+var3=77.9
+var4="32"
+
+print(var1+var4)
+
+print(int(var1)+int(var4))
+
+"""
+output-->
+
+5432
+86
+
+"""
+#_________________________________________________________________________________________________
+
+var1 ="hello world"
+print(5*var1)
+
+"""
+output-->
+hello worldhello worldhello worldhello worldhello world
+
+"""
+#_________________________________________________________________________________________________
+
+print("Enter your number")
+
+inpnum = input()
+
+# it will take string as input
+
+print("after add 10 in the number result will be", int(inpnum)+10)
+
+
+"""
+output
+Enter your number
+78
+after add 10 in the number result will be 88
+
+"""
